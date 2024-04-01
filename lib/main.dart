46029361d45e6c418b96799079fbb3ad98f17470
arjun40779 <arjun40779@gmail.com>
2024-04-01 18:29:22 +0530

    navigation bar completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,71 +1,168 @@
 import 'package:flutter/material.dart';
-import 'components/navigation_bar.dart';
-import 'pages/bookmarks.dart';
+import 'package:lazy_load_indexed_stack/lazy_load_indexed_stack.dart';
 import 'pages/home.dart';
 import 'pages/messages.dart';
 import 'pages/notification_page.dart';
 import 'pages/profile.dart';
-import 'components/custom_button.dart';
-import 'theme/app_theme.dart';
-import '/utils/size_config.dart';
+import 'utils/size_config.dart';
 
-void main() {
+void main() async {
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+    SizeConfig().init(context);
     return MaterialApp(
-      title: 'Flutter Demo',
       debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        textButtonTheme: TextButtonThemeData(
-            style: ButtonStyle(
-                foregroundColor: MaterialStateProperty.all(Colors.black))),
-        appBarTheme:
-            const AppBarTheme(iconTheme: IconThemeData(color: Colors.white)),
-        fontFamily: 'poppins',
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(title: 'My App'),
+      routes: {
+        '/mainlayout': (context) => const MainLayout(),
+        '/profile': (context) => const ProfilePage(),
+        '/messages': (context) => const Messages(),
+        '/notification': (context) => const NotificationPage(),
+      },
+      initialRoute: '/mainlayout',
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-  final String title;
+class MainLayout extends StatefulWidget {
+  const MainLayout({super.key});
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  State<MainLayout> createState() => _MainLayoutState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int currPageIndex = 0;
-  void changePageIndex(int ind) {
-    setState(() {
-      currPageIndex = ind;
-    });
-  }
+class _MainLayoutState extends State<MainLayout> {
+  int _currentIndex = 0;
+
+  final _page1 = GlobalKey<NavigatorState>();
+  final _page2 = GlobalKey<NavigatorState>();
+  final _page3 = GlobalKey<NavigatorState>();
+  final _page4 = GlobalKey<NavigatorState>();
 
   @override
   Widget build(BuildContext context) {
-    SizeConfig().init(context);
-
     return Scaffold(
-        bottomNavigationBar: NavBar(
-            currPageIndex: currPageIndex, changePageIndex: changePageIndex),
-        body: const <Widget>[
-          HomePage(),
-          NotificationPage(),
-          ProfilePage(),
-          Messages(),
-          Bookmarks()
-        ][currPageIndex]);
+      body: LazyLoadIndexedStack(
+        index: _currentIndex,
+        children: [
+          Navigator(
+            key: _page1,
+            onGenerateRoute: (route) => MaterialPageRoute(
+              settings: route,
+              builder: (context) => const HomePage(),
+            ),
+          ),
+          Navigator(
+            key: _page2,
+            onGenerateRoute: (route) => MaterialPageRoute(
+              settings: route,
+              builder: (context) => const ProfilePage(),
+            ),
+          ),
+          Navigator(
+            key: _page3,
+            onGenerateRoute: (route) => MaterialPageRoute(
+              settings: route,
+              builder: (context) => const Messages(),
+            ),
+          ),
+          Navigator(
+            key: _page4,
+            onGenerateRoute: (route) => MaterialPageRoute(
+              settings: route,
+              builder: (context) => const NotificationPage(),
+            ),
+          ),
+        ],
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        backgroundColor: Colors.white,
+        currentIndex: _currentIndex,
+        onTap: (index) {
+          if (_currentIndex == index && _getCurrentNavigator()!.canPop()) {
+            _getCurrentNavigator()!.popUntil((route) => route.isFirst);
+          } else {
+            setState(() {
+              _currentIndex = index;
+            });
+          }
+        },
+        type: BottomNavigationBarType.fixed,
+        selectedItemColor: Colors.redAccent,
+        unselectedItemColor: Colors.grey,
+        showSelectedLabels: false,
+        showUnselectedLabels: false,
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
+          BottomNavigationBarItem(icon: Icon(Icons.person), label: 'Profile'),
+          BottomNavigationBarItem(icon: Icon(Icons.message), label: 'Messages'),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.notifications), label: 'Notifications'),
+        ],
+      ),
+    );
+  }
+
+  NavigatorState? _getCurrentNavigator() {
+    switch (_currentIndex) {
+      case 0:
+        return _page1.currentState;
+      case 1:
+        return _page2.currentState;
+      case 2:
+        return _page3.currentState;
+      case 3:
+        return _page4.currentState;
+      default:
+        return null;
+    }
+  }
+}
+
+class Page1 extends StatelessWidget {
+  const Page1({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+        child: TextButton(
+      onPressed: () {
+        Navigator.push(
+            context, MaterialPageRoute(builder: (context) => Page2()));
+      },
+      child: Text("goto page2"),
+    ));
+  }
+}
+
+class Page2 extends StatelessWidget {
+  const Page2({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+        child: TextButton(
+      onPressed: () {
+        Navigator.push(
+            context, MaterialPageRoute(builder: (context) => Page3()));
+      },
+      child: Text("goto page3"),
+    ));
+  }
+}
+
+class Page3 extends StatelessWidget {
+  const Page3({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text("page3"),
+    );
   }
 }
--- lib/pages/messages.dart
+++ lib/pages/messages.dart
@@ -1,10 +1,19 @@
 import 'package:flutter/material.dart';
+import '../main.dart';
+
+import 'home.dart';
 
 class Messages extends StatelessWidget {
   const Messages({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Center(child: Text("Messages"));
+    return Center(
+        child: TextButton(
+            onPressed: () {
+              Navigator.push(
+                  context, MaterialPageRoute(builder: (context) => Page1()));
+            },
+            child: const Text("goto page1")));
   }
 }
